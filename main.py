28f8f9e42f00d9d54b418ac3435f4ca86ede43fd
shaun mcgrath <shaun.mcgrath@madefore.com.au>
2021-10-19 20:29:33 +1100

    tinker

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import socket
 import argparse
 
 GET_ALL_DEVICES="81"
+SET_SWITCH_STATE="82"
 
 
 def send_cmd(s, sn, cmd):
@@ -32,17 +33,40 @@ def send_cmd(s, sn, cmd):
             print(name + ": " + status + ", short: 0x" + short.to_bytes(2, byteorder='big').hex() + ", ep: 0x" + ep + online_status)
             b = s.recv(2)
 
+    if cmd[0] == int(SET_SWITCH_STATE, 16):
+        b = s.recv(2)
+        while len(b) == 2 and b[0] == 1:
+            b = s.recv(b[1])
+            short=int.from_bytes(b[0:2], byteorder='little')
+            ep=b[2:3].hex()
+            if b[7] == 1:
+                status="on"
+            else:
+                status="off"
+            name=b[9:9+b[8]].decode()
+            if b[9+b[8]] == 0:
+                online_status=" (offline)"
+            else:
+                online_status=""
+            if name == "":
+                name = "[" + b[19:19+b[18]].decode() + "]"
+            print(name + ": " + status + ", short: 0x" + short.to_bytes(2, byteorder='big').hex() + ", ep: 0x" + ep + online_status)
+            b = s.recv(2)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Talk to hub!')
     parser.add_argument('--ip', '-i', dest='ip', nargs=1, required=True)
     parser.add_argument('--port', '-p', dest='port', type=int, nargs=1, required=True)
     parser.add_argument('--serial-number', '-s', dest='sn', nargs=1, required=True)
+    parser.add_argument('--raw', '-r', dest='raw', nargs=1, required=True)
     args = parser.parse_args()
 
     sn = bytes.fromhex(args.sn[0])
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     s.connect((args.ip[0], args.port[0]))
-    send_cmd(s, sn, GET_ALL_DEVICES)
+    ##send_cmd(s, sn, GET_ALL_DEVICES)
+    send_cmd(s, sn, args.raw[0])
     s.close()
 
 if __name__ == "__main__":
